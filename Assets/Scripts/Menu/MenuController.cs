b1e79fc2f08845119e841f916606581a8a5a6302
Hector Alvarez <winman1988@gmail.com>
2017-11-27 21:42:08 -0300

    Menú Editado

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -2,13 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MenuController : MonoBehaviour {
+public class MenuController : MonoBehaviour 
+{
+	public AudioSource aullido;
 
-	public void StartGame(string p) {
-		SceneManager.LoadScene (p);
+	public void Start()
+	{
 	}
-	public void ExitGame () {
+
+	public void Update()
+	{
+	}
+
+	public void StartGame(int p) 
+	{
+		//AudioSource audio = GetComponent<AudioSource> ();
+		//audio.clip = aullido;
+		aullido.Play ();
+
+		StartCoroutine( WaitAndLoadScene ( aullido.clip.length, p) );
+
+		//WaitForSeconds (2);
+
+
+	}
+
+	public void ExitGame () 
+	{
 		#if UNITY_EDITOR
 			UnityEditor.EditorApplication.isPlaying = false;
 		#else
@@ -16,4 +38,10 @@ public class MenuController : MonoBehaviour {
 		#endif
 	}
 
+	IEnumerator WaitAndLoadScene(float num, int scene)
+	{
+		//Debug.Log ("Wait? " + num);
+		yield return new WaitForSeconds (num);
+		SceneManager.LoadScene (scene);
+	}
 }
