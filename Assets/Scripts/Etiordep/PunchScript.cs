de316d2bedf79d3dddafe5e5bbd45c3138f6989c
Hector Alvarez <winman1988@gmail.com>
2017-12-02 17:00:42 -0300

    Escenario de pruebas para personajes, prefab de otirdep con sonido

--- Assets/Scripts/Etiordep/PunchScript.cs
+++ Assets/Scripts/Etiordep/PunchScript.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PunchScript : MonoBehaviour {
+	public AudioSource punchSound;
+
+	private bool isPunch = false;
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +15,17 @@ public class PunchScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.J)) {
+			if(!isPunch)
+				punchSound.Play ();
+
+			isPunch = true;
 			GetComponent<Animator> ().SetBool ("isPunch", true);
 		}
 		if (Input.GetKeyUp(KeyCode.J)) {
+			if(isPunch)
+				punchSound.Stop ();
+			
+			isPunch = false;
 			GetComponent<Animator> ().SetBool ("isPunch", false);
 		}
 	}
