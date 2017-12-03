35106aecfe49eebb0bc9011a1fe60b43cc230f1a
DanielCoronado <danielcoronado@hotmail.cl>
2017-12-03 19:26:22 -0300

    mejora de escenarios

---
+++ Assets/Scripts/Sitnam/EnemyFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+	public Transform target;
+	public float enemySpeed = 0.5f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+		transform.position = Vector2.Lerp (transform.position, target.position, enemySpeed * Time.deltaTime);
+	}
+
+}
