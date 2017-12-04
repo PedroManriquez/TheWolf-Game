137733d286d1e0eb273994304ddda64f9e96ccde
Hector Alvarez <winman1988@gmail.com>
2017-12-04 00:42:58 -0300

    Cambios de vida en xineohp

--- Assets/Scripts/Xineohp/XWalkScript.cs
+++ Assets/Scripts/Xineohp/XWalkScript.cs
@@ -16,14 +16,14 @@ public class XWalkScript : MonoBehaviour {
 				GetComponent<SpriteRenderer>().flipX = false;
 			}
 			GetComponent<Animator>().SetBool ("isWalking", true);
-			transform.Translate (0.09f, 0, 0);
+			transform.Translate (0.12f, 0, 0);
 		}
 		if (Input.GetKey(KeyCode.LeftArrow)) {
 			if (GetComponent<SpriteRenderer>().flipX == false) {
 				GetComponent<SpriteRenderer>().flipX = true;
 			}
 			GetComponent<Animator> ().SetBool ("isWalking", true);
-			transform.Translate (-0.09f, 0, 0);
+			transform.Translate (-0.12f, 0, 0);
 			
 		}
 		// Disabled animator
