321ed88397fe43e80f107b87846cdae48fa02e57
DanielCoronado <danielcoronado@hotmail.cl>
2017-12-04 11:28:36 -0300

    Fixed credits and history

---
+++ Assets/Scripts/CreditsHistory/CreditsHistoryController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsHistoryController : MonoBehaviour {
+
+	public float tiempo;
+	public int p;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine( WaitAndLoadScene ( tiempo, p) );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator WaitAndLoadScene(float tiempo, int scene)
+	{
+		yield return new WaitForSeconds (tiempo);
+		SceneManager.LoadScene (scene);
+	}
+}
