c13244589f78a7d59e5b9542b684651379c8758b
PedroManriquez <ptrmanriquez@gmail.com>
2017-12-04 03:20:43 -0300

    Added sprites actions in aliuga

--- Assets/Scripts/Aliuga/AliugaBehaviourScript.cs
+++ Assets/Scripts/Aliuga/AliugaBehaviourScript.cs
@@ -10,47 +10,58 @@ public class AliugaBehaviourScript : MonoBehaviour {
 
 	public int lives = 1;
 	public int energy = 150;
-
+	public Animator anim;
+	private bool live;
+	private bool isQuiet;
 	void Start () {
 		Player = GameObject.Find ("Etiordep");
 		startingPositionX = transform.position;
+		anim = GetComponent<Animator> ();
+		live = true;
+		isQuiet = true;
 	}
 
 	void FixedUpdate () {
-
-
-		if (Mathf.Abs (transform.position.x - Player.transform.position.x) < 4f) {
-
-
-			transform.position = Vector2.MoveTowards (transform.position, Player.transform.position, enemySpeed * Time.deltaTime);
-
-			if (transform.position.x > Player.transform.position.x) {
-				GetComponent<SpriteRenderer> ().flipX = true;
-			} else {
-				GetComponent<SpriteRenderer> ().flipX = false;
-			}
-
-		} else {
-
-			if (transform.position.x != startingPositionX.x) {
-				if (transform.position.x > startingPositionX.x) {
+		if (isQuiet) {
+			anim.SetBool("isFly", false);
+		}
+		if (live) {
+			if (Mathf.Abs (transform.position.x - Player.transform.position.x) < 4f) {
+				anim.SetBool("isFly", true);
+				isQuiet = false;
+				transform.position = Vector2.MoveTowards (transform.position, Player.transform.position, enemySpeed * Time.deltaTime);
+				if (transform.position.x > Player.transform.position.x) {
 					GetComponent<SpriteRenderer> ().flipX = true;
 				} else {
 					GetComponent<SpriteRenderer> ().flipX = false;
 				}
-
-				transform.position = Vector2.MoveTowards (transform.position, startingPositionX, enemySpeed * 2 * Time.deltaTime);
 			} else {
-				GetComponent<SpriteRenderer> ().flipX = true;
+
+				if (transform.position.x != startingPositionX.x) {
+					if (transform.position.x > startingPositionX.x) {
+						GetComponent<SpriteRenderer> ().flipX = true;
+					} else {
+						GetComponent<SpriteRenderer> ().flipX = false;
+						isQuiet = true;
+					}
+					anim.SetBool("isFly", true);
+					isQuiet = false;
+					transform.position = Vector2.MoveTowards (transform.position, startingPositionX, enemySpeed * 2 * Time.deltaTime);
+				} else {
+					GetComponent<SpriteRenderer> ().flipX = true;
+				}
 			}
 		}
+		
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
 		if (col.gameObject.tag == "Player") {
 			energy = energy - 25;
 			if (energy == 0) {
-				Destroy (gameObject);
+				live = false;
+				anim.SetBool("isDie", true);
+				Destroy (gameObject, 1.4f);
 			}
 		}
 	}
