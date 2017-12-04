6c4ce4e81d1ba363cf00167d4846c37f1116f5f0
MrSenk <camilo.c_n@hotmail.com>
2017-12-03 21:57:51 -0300

    Fixed EnemyFollows
    Sitnam follows and flips in the direction of the character

--- Assets/Scripts/Sitnam/EnemyFollow.cs
+++ Assets/Scripts/Sitnam/EnemyFollow.cs
@@ -6,13 +6,21 @@ public class EnemyFollow : MonoBehaviour {
 
 	public float enemySpeed = 0.5f;
 	private GameObject Player;
+	private Rigidbody2D rigi2D;
 
 	void Start () {
 		Player = GameObject.Find ("Etiordep");
+		rigi2D = GetComponent<Rigidbody2D> ();
 	}
 
 	void Update () {
 		transform.position = Vector2.MoveTowards (transform.position, Player.transform.position, enemySpeed * Time.deltaTime);
+
+		if (transform.position.x > Player.transform.position.x) {
+			GetComponent<SpriteRenderer> ().flipX = true;
+		} else {
+			GetComponent<SpriteRenderer> ().flipX = false;
+		}
 	}
 
 }
