2484337738ae3c5f464559c63dd47fc6ffdc45d9
MrSenk <camilo.c_n@hotmail.com>
2017-12-03 22:39:38 -0300

    Added OnCollision for Etiordep
    Added OnCollision method and a Text element that displays the character's health, has to be changed for animated sprites in the future

--- Assets/Scripts/Etiordep/EtiordepController.cs
+++ Assets/Scripts/Etiordep/EtiordepController.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EtiordepController : MonoBehaviour {
 
+	private int salud = 10;
+	public Text saludText;
+
 	// Use this for initialization
 	void Start () {
-		
+		saludText.text = "Salud: " + salud;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Enemy") {
+			salud = salud - 1;
+			saludText.text = "Salud: " + salud;
+		}
 	}
 }
